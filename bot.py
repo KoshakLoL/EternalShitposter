caeda67509e751c310ee9988e6b6ebf0a4bf69ce
Cosmos <53345275+sadneko@users.noreply.github.com>
2020-12-03 23:39:44 +0300

    Added a "fortune" command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import vk_api
+import subprocess
 from random import choice
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
@@ -27,21 +28,28 @@ class MainBot:
                         message="Please add me to a group chat, I don't work in private messages!"
                     )
                 else:
-                    self.score = self.db.get_score(msg_recipient)
-                    if self.group_id in event.obj.text or self.score == 10:
-                        final = (get_file_array("array1.txt") +
-                                 get_file_array("array2.txt") +
-                                 get_file_array("array3.txt")).replace("\n", "")
-
+                    if "fortune" in event.obj.text and self.group_id in event.obj.text :
                         self.vk.messages.send(
-                            peer_id=msg_recipient,
-                            random_id=get_random_id(),
-                            message=final
-                        )
-                        self.score = 1
+                        peer_id=msg_recipient,
+                        random_id=get_random_id(),
+                        message=subprocess.check_output(['fortune', '-eso'])
+                    )
                     else:
-                        self.score += 1
-                    self.db.update_chat(str(event.obj.peer_id), self.score)
+                        self.score = self.db.get_score(msg_recipient)
+                        if self.group_id in event.obj.text or self.score == 10:
+                            final = (get_file_array("array1.txt") +
+                                    get_file_array("array2.txt") +
+                                    get_file_array("array3.txt")).replace("\n", "")
+
+                            self.vk.messages.send(
+                                peer_id=msg_recipient,
+                                random_id=get_random_id(),
+                                message=final
+                            )
+                            self.score = 1
+                        else:
+                            self.score += 1
+                        self.db.update_chat(str(event.obj.peer_id), self.score)
 
 
 def get_file_array(file):
@@ -49,7 +57,7 @@ def get_file_array(file):
         return choice(list(array)) + " "
 
 
-database = DataBase("database")
-bot = MainBot("token", "group_id", database)
+database = DataBase(database)
+bot = MainBot(token, group, database)
 if __name__ == "__main__":
     bot.main()
