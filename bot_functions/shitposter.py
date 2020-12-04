a9f71657c5671d2ba5e7f501c5e4583817546e1e
Cosmos <53345275+sadneko@users.noreply.github.com>
2020-12-04 21:00:52 +0300

    fixed shitpost generation
    lmao come on who will read this

--- bot_functions/shitposter.py
+++ bot_functions/shitposter.py
@@ -2,9 +2,9 @@ from random import choice
 
 
 def shitpost():
-    return (get_file_array("../array1.txt") +
-            get_file_array("../array2.txt") +
-            get_file_array("../array3.txt")).replace("\n", "")
+    return (get_file_array("array1.txt") +
+            get_file_array("array2.txt") +
+            get_file_array("array3.txt")).replace("\n", "")
 
 
 def get_file_array(file):
