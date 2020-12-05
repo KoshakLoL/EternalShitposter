1cb539a03706f9fc0fd8b47a9a93425988c21a22
KoshakLoL <motorealinfo@gmail.com>
2020-12-05 23:13:08 +0500

    Now only admins can set the auto-shitpost mode

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ class MainBot(MainFunc):
         for event in self.get_event_listener():
             if event.type == VkBotEventType.MESSAGE_NEW:
                 self.set_message_recipient(event)
-                if self.msg_recipient < 2000000000:
+                if event.from_user:
                     self.not_group()
                 elif self.group_name in event.obj.text:
                     if "fortune" in event.obj.text:
@@ -21,12 +21,17 @@ class MainBot(MainFunc):
                         self.add_score()
                         self.check_score()
                     elif "auto" in event.obj.text:
-                        if self.auto_shitpost == 0:
-                            self.set_database_status(1)
-                            self.set_message_payload("Now the bot will auto-shitpost!")
+                        response = self.vk_api.messages.getConversationMembers(peer_id=self.msg_recipient,
+                                                                               group_id=self.group_id)["items"]
+                        if check_for_owner(response, event):
+                            if self.auto_shitpost == 0:
+                                self.set_database_status(1)
+                                self.set_message_payload("Now the bot will auto-shitpost!")
+                            else:
+                                self.set_database_status(0)
+                                self.set_message_payload("Now the bot will NOT auto-shitpost... You're no fun :(")
                         else:
-                            self.set_database_status(0)
-                            self.set_message_payload("Now the bot will NOT auto-shitpost... You're no fun :(")
+                            self.set_message_payload("You're not an admin bro, get yourself some moderating privileges")
                     else:
                         self.shitpost()
                         self.reset_score()
@@ -36,6 +41,14 @@ class MainBot(MainFunc):
                     self.check_score()
                 self.set_database_score()
                 self.set_database_status(self.get_status())
+
+
+def check_for_owner(response, event):
+    for entry in response:
+        if entry["member_id"] == event.obj.from_id:
+            if ("is_admin" in entry and entry["is_admin"]) or ("is_owner" in entry and entry["is_owner"]):
+                return True
+    return False
 
 
 if __name__ == "__main__":
