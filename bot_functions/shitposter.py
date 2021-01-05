9b52aad977b46cb61dcf5479f61d6be498378a29
KoshakLoL <motorealinfo@gmail.com>
2021-01-05 19:09:56 +0500

    Env Token, FStrings, correct packages, gitignore, speed

--- bot_functions/shitposter.py
+++ bot_functions/shitposter.py
@@ -2,11 +2,11 @@ from random import choice
 
 
 def shitpost():
-    return (get_file_array("arrays/array1.txt") +
-            get_file_array("arrays/array2.txt") +
-            get_file_array("arrays/array3.txt")).replace("\n", "")
+    return (f"{get_file_array('arrays/array1.txt')}"
+            f" {get_file_array('arrays/array2.txt')}"
+            f" {get_file_array('arrays/array3.txt')}".replace("\n", ""))
 
 
 def get_file_array(file):
     with open(file, "r") as array:
-        return choice(list(array)) + " "
+        return choice(list(array))
--- database.py
+++ database.py
@@ -4,33 +4,27 @@ import sqlite3
 class DataBase:
     def __init__(self, database_file):
         if ".db" not in database_file:
-            raise NoDatabaseFile
+            print("No database file in bot.py! Please add one.")
+            exit()
         self.db = sqlite3.connect(database_file)
         self.cur = self.db.cursor()
 
     def update_value(self, table, column, chat_id, value):
         with self.db:
-            if self.cur.execute("SELECT EXISTS(SELECT * FROM {} WHERE chat=:chat)".format(table),
+            if self.cur.execute(f"SELECT EXISTS(SELECT * FROM {table} WHERE chat=:chat)",
                                 {"chat": chat_id}).fetchone() == (1,):
-                self.cur.execute("""UPDATE {} SET {} = :value
-                                     WHERE chat = :chat
-                """.format(table, column), {"value": value, "chat": chat_id})
+                self.cur.execute(f"UPDATE {table} SET {column} = :value WHERE chat = :chat",
+                                 {"value": value, "chat": chat_id})
             else:
-                self.cur.execute("INSERT INTO {} VALUES (:chat, :value)".format(table),
+                self.cur.execute(f"INSERT INTO {table} VALUES (:chat, :value)",
                                  {"chat": chat_id, "value": value})
 
     def get_value(self, table, chat_id):
-        self.cur.execute("SELECT * FROM {} WHERE chat=:chat".format(table), {"chat": chat_id})
+        self.cur.execute(f"SELECT * FROM {table} WHERE chat=:chat", {"chat": chat_id})
         try:
             return self.cur.fetchone()[1]
         except TypeError:
             return 1
 
-    def close_db(self):
+    def __del__(self):
         self.db.close()
-
-
-class NoDatabaseFile(Exception):
-    def __init__(self):
-        print("No database file in bot.py! Please add one.")
-        exit()
