6553144d068b91cb2e283118fabc5111d96405bb
KoshakLoL <motorealinfo@gmail.com>
2021-01-25 00:06:38 +0500

    Auto-shitpost limiter, optimization

--- bot_utils.py
+++ bot_utils.py
@@ -12,18 +12,6 @@ def msg_construct(vk, msg_recipient, message):
     )
 
 
-def check_for_owner(response, event):
-    for entry in response:
-        # If it is the same member as the one who called
-        # If the member is an owner or an admin
-        if entry["member_id"] == event.obj.from_id and (
-                ("is_admin" in entry and entry["is_admin"]) or
-                ("is_owner" in entry and entry["is_owner"])
-        ):
-            return True
-    return False
-
-
 def get_yml_logger(config, name):
     with open(config, "r") as f:
         logging.config.dictConfig(yaml.safe_load(f))
